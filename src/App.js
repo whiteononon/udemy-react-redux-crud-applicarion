59dd8b5a6aaaaf4967270d866a5ae60c00302e93
whiteononon <whiteon0618@gmail.com>
2020-09-04 20:58:34 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from "react";
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//       <label htmlFor="bar">Bar</label>
+//       <input
+//         type="text"
+//         onChange={() => {
+//           console.log("hi");
+//         }}
+//       />
+//     </>
+//   );
+// }
+
+const App = () => {
   return (
-    <>
-      <label htmlFor="bar">Bar</label>
-      <input
-        type="text"
-        onChange={() => {
-          console.log("hi");
-        }}
-      />
-    </>
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   );
-}
+};
+
+const Cat = () => {
+  return <div>mewo</div>;
+};
 
 export default App;
